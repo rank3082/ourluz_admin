5cc74ac2381d3344b170bcbe13f5a8458bac3410
omrikellner <omrikellner6@gmail.com>
2023-08-23 18:57:44 +0300

    create button for send email

--- src/pages/weeklyBookedCalander/WeeklyBookedCalender.tsx
+++ src/pages/weeklyBookedCalander/WeeklyBookedCalender.tsx
@@ -7,15 +7,23 @@ import {WeeklyBookedRow} from "./components/weeklyBookedRow/WeeklyBookedRow";
 import { useParams } from 'react-router-dom';
 import {setToken} from "../../store/authentication.slice";
 import {useDispatch} from "react-redux";
+import {Views} from "react-big-calendar";
+import {Button} from "@mui/material";
+import {setSelectedPage} from "../../store/global.slice";
+import {SelectedPage} from "../../utils/enum.const";
 
 export const WeeklyBookedCalender=()=>{
     const dispatch= useDispatch()
-    const {weeklyEventList} = useAppSelector(state => state.global);
-    const { to, from } = useParams();
+    const {weeklyEventList,isAdmin,weekDates} = useAppSelector(state => state.global);
+    let { to, from } = useParams();
+    if (weekDates?.start && weekDates?.end){
+         from = moment(weekDates.start).format("yyyy-MM-D")
+         to = moment(weekDates.end).format("yyyy-MM-D")
+    }
     const checkIfUserConnected= async (storageUsername:string,storagePassword:string,token:string)=>{
         dispatch(setToken(token))
     }
-
+    console.log(weeklyEventList,"weeklyEventList")
     useEffect( ()=>{
             const storageUsername = localStorage.getItem("username");
             const token = localStorage.getItem("token");
@@ -39,8 +47,20 @@ export const WeeklyBookedCalender=()=>{
         columns.push(`${formattedDate} ${dayOfWeek}`);
         currentDate.add(1, "day");
     }
-
-    return <div  className={"tableWrapper"}>
+    console.log(columns,"columns")
+    return <div>
+        {isAdmin  && <Button
+            className={"addEventButtonNotSelected"}
+            // onClick={()=>window.open(`/weeklyBooking/from/${startDate}/to/${endDate}`)}>
+            onClick={()=>dispatch(setSelectedPage(SelectedPage.MainPanel))}>
+            חזור למסך הבית
+        </Button>}
+        <div style={{display:"flex", gap:"10%",padding:10,justifyContent:"center",alignItems:"center",textAlign:"center"}}>
+           <div style={{fontWeight:700,fontSize:28}}>{` ${moment(from).format("MM/DD")} - ${moment(to).format("MM/DD")} `}</div>
+           <div style={{fontSize:28,fontWeight:700,color:"var(--primary)"}}>לוח משמרות עבור שבוע </div>
+        </div>
+        <div className={"greyLine"}></div>
+        <div  className={"tableWrapper"}>
         <table style={{borderCollapse: 'collapse',direction:"rtl",width:"100%",height:"100%"}}>
             <thead style={{position: "sticky", top: 0}}>
             <tr style={{paddingBlock: 24}}>
@@ -59,5 +79,6 @@ export const WeeklyBookedCalender=()=>{
             })}
             </tbody>
         </table>
-    </div>
+    </div></div>
+
 }
